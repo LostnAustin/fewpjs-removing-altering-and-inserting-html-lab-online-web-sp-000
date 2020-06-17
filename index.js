03bd6e99e425e734740da2f986120061dd73db71
Chad Lengefeld <chadlengefeld@gmail.com>
2020-06-17 08:56:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,5 +16,12 @@
 main.remove()
 
 let newHeader = document.createElement('newHeader');
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li')
+  li.innerHTML = (i + 1).toString()
+  ul.appendChild(li)
+}
+ 
+element.appendChild(newHeader)
 // ('h1#victory');
 // element.innerHTML = 'YOUR-NAME is the champion';
