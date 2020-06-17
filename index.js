7610d175b51f12942d1fcbc2e180ad5ff1d4fd97
Chad Lengefeld <chadlengefeld@gmail.com>
2020-06-17 09:06:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ main.remove()
 
 let newHeader = document.createElement('h1');
 newHeader.id = 'victory';
-newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.innerHTML = "Chad is the champion!";
 
 
 
