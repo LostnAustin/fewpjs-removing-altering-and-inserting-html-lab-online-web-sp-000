e82550a67df20c46e4aa7cff6c96dfc8ecc715e2
Chad Lengefeld <chadlengefeld@gmail.com>
2020-06-17 09:07:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,15 +19,3 @@ main.remove()
 let newHeader = document.createElement('h1');
 newHeader.id = 'victory';
 newHeader.innerHTML = "Chad is the champion!";
-
-
-
-// for (let i = 0; i < 3; i++) {
-//   let li = document.createElement('li')
-//   li.innerHTML = (i + 1).toString()
-//   ul.appendChild(li)
-// }
-
-// element.appendChild(h1#victory)
-// ('h1#victory');
-// element.innerHTML = 'YOUR-NAME is the champion';
